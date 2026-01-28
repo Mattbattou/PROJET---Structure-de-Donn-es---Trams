8cd6637ed1f5cdd07088a294c8782d0e20559ae7
Mattbatt <56692381+Mattbattou@users.noreply.github.com>
2026-01-28 10:26:53 +0000

    Ajout variables privées

--- src/classes/arrets/arret.h
+++ src/classes/arrets/arret.h
@@ -1,10 +1,19 @@
 #ifndef ARRET_H
 #define ARRET_H
 
+#include<string>
+
+using namespace std;
+
+
+
 class arret
 {
     private:
-    
+        int d_x;
+        int d_y;
+        string d_nom;
+        int tempsArretMinimum; // En secondes
 
     public:
 
